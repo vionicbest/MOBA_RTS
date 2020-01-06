559f0a14271f52bf07b9c3b318dcb2493e8d5dae
vionicbest <vionicbest@snu.ac.kr>
2020-01-06 15:25:21 +0900

    add character and tilemap

---
+++ MOBA_RTS/Assets/Scripts/Character.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public enum CharacterDirection
+    {
+        Left,
+        Right,
+    };
+    [SerializeField]
+    int characterCode;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    Sprite spriteLeft, spriteRight;
+
+    public float finalSpeed()
+    {
+        return speed;
+    }
+
+    public void changeSprite(CharacterDirection direction)
+    {
+        switch(direction)
+        {
+            case CharacterDirection.Left:
+                GetComponent<SpriteRenderer>().sprite = spriteLeft;
+                break;
+            case CharacterDirection.Right:
+                GetComponent<SpriteRenderer>().sprite = spriteRight;
+                break;
+        }
+    }
+}
---
+++ MOBA_RTS/Assets/Scripts/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField]
+    Camera Camera;
+    [SerializeField]
+    Character hero;
+
+    Vector3 nextMovePosition;
+    void Start()
+    {
+        Camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.Mouse0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            nextMovePosition = Camera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, -Camera.main.transform.position.z));
+        }
+        if (hero.transform.position != nextMovePosition)
+        {
+            float speed = hero.finalSpeed() * Time.deltaTime;
+            hero.transform.position = Vector3.MoveTowards(hero.transform.position, nextMovePosition, speed);
+            if (hero.transform.position.x > nextMovePosition.x)
+            {
+                hero.changeSprite(Character.CharacterDirection.Left);
+            }
+            else if (hero.transform.position.x < nextMovePosition.x)
+            {
+                hero.changeSprite(Character.CharacterDirection.Right);
+            }
+        }
+    }
+}
